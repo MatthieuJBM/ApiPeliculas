b2e5c47552ed0e0139df80c0eb9e5704d0e237f7
Mateusz Baryla <m.baryla@protonmail.ch>
2025-05-01 16:50:21 +0200

    feat: Crear nueva categoria (HTTP POST).

--- ApiPeliculas/Controllers/CategoriasController.cs
+++ ApiPeliculas/Controllers/CategoriasController.cs
@@ -1,3 +1,4 @@
+using ApiPeliculas.Models;
 using ApiPeliculas.Models.Dtos;
 using ApiPeliculas.Repository.IRepository;
 using AutoMapper;
@@ -22,8 +23,8 @@ public class CategoriasController : ControllerBase
     }
 
     [HttpGet]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public IActionResult GetCategorias()
     {
         var listaCategorias = _ctRepo.GetCategorias();
@@ -37,10 +38,10 @@ public class CategoriasController : ControllerBase
     }
 
     [HttpGet("{categoriaId:int}", Name = "GetCategoria")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetCategoria(int categoriaId)
     {
         var itemCategoria = _ctRepo.GetCategoria(categoriaId);
@@ -53,5 +54,39 @@ public class CategoriasController : ControllerBase
         // Cada vez tenemos que exponer el DTO y no el nuestro modelo.
         var itemCategoriaDto = _mapper.Map<CategoriaDto>(itemCategoria);
         return Ok(itemCategoriaDto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult CrearCategoria([FromBody] CrearCategoriaDto crearCategoriaDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (crearCategoriaDto == null)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (_ctRepo.ExisteCategoria(crearCategoriaDto.Nombre))
+        {
+            ModelState.AddModelError("Nombre", $"La categoría ya existe");
+            return StatusCode(404, ModelState);
+        }
+
+        var categoria = _mapper.Map<Categoria>(crearCategoriaDto);
+
+        if (!_ctRepo.CreateCategoria(categoria))
+        {
+            ModelState.AddModelError("", $"Algo salió mal guardando el registro {categoria.Nombre}");
+            return StatusCode(404, ModelState);
+        }
+
+        return CreatedAtRoute("GetCategoria", new { categoriaId = categoria.Id }, categoria);
     }
 }
